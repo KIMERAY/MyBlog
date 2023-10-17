fa46966cb4022322647c18ea11b375d3cace1b35
kimeray <kimerayforw@outlook.com>
2023-10-18 02:32:58 +0800

    1

--- MyBlog.JWT/Controllers/AuthoizeController.cs
+++ MyBlog.JWT/Controllers/AuthoizeController.cs
@@ -1,7 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MyBlog.IService;
+using MyBlog.JWT.Utility._MD5;
 using MyBlog.JWT.Utility.ApiResult;
 
 namespace MyBlog.JWT.Controllers
@@ -13,13 +19,42 @@ namespace MyBlog.JWT.Controllers
         private readonly IWriterInfoService _iWriterInfoService;
         public AuthoizeController(IWriterInfoService iWriterInfoService)
         {
-            
+            _iWriterInfoService = iWriterInfoService;
         }
         [HttpPost("Login")]
         public async Task<ApiResult> Login(string username, string userpwd)
         {
+            //加密后的密码
+            string pwd = MD5Helper.MD5Encrypt32(userpwd);
             //数据校验
-            
+            var writer = await _iWriterInfoService.FindAsync(c => c.UserName == username && c.UserName == pwd);
+            if (writer!=null)
+            {
+                //登陆成功
+                var claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, writer.Name),
+                    new Claim("Id", writer.Id.ToString()),
+                    new Claim("UserName", writer.UserName)
+                    //不能放敏感信息
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SDMC-CJAS1-SAD-DFSFA-SADHJVF-VF"));
+                //issuer代表办法Token的Web应用程序，audience是Token的受理者
+                var token = new JwtSecurityToken(
+                    issuer: "http://localhost:6060",
+                    audience: "http://localhost:5000",
+                    claims: claims,
+                    notBefore: DateTime.Now,
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                );
+                var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+                return ApiResultHelper.Success(jwtToken);
+            }
+            else
+            {
+                return ApiResultHelper.Error("账号哦或密码错误");
+            }
         }
     }
 }
--- MyBlog.JWT/Startup.cs
+++ MyBlog.JWT/Startup.cs
@@ -6,6 +6,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MyBlog.IRepository;
+using MyBlog.IService;
+using MyBlog.Repository;
+using MyBlog.Service;
+using SqlSugar.IOC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +36,18 @@ namespace MyBlog.JWT
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyBlog.JWT", Version = "v1" });
             });
+            #region SqlSugarIOC
+
+            services.AddSqlSugar(new IocConfig()
+            {
+                ConnectionString = this.Configuration["SqlConn"],
+                DbType = IocDbType.SqlServer,
+                IsAutoCloseConnection = true
+            });
+
+            #endregion
+            services.AddScoped<IWriterInfoRepository, WriterInfoRepository>();
+            services.AddScoped<IWriterInfoService, WriterInfoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
